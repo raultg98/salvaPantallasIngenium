8483212fb9afe488ae5882e89b4e0976bda9ef26
raultg98 <raul.frek@gmail.com>
2021-06-17 12:08:25 +0200

    Añadiendo animaciones de carga y de subida de salvapantallas

---
+++ src/public/js/cargaWebIndex.js
@@ -0,0 +1,15 @@
+// const boton = document.getElementById('btnSubmit');
+const contenedor = document.getElementById('contenedor-carga');
+const cargaLetras = document.getElementById('carga-letras');
+
+// boton.addEventListener('click', () => {
+//     contenedor.style.visibility = '';
+//     contenedor.style.opacity = '0.9';
+//     cargaLetras.innerText = 'PROCESANDO CAMBIOS';
+// });
+
+window.onload = function(){
+    contenedor.style.visibility = 'hidden';
+    contenedor.style.opacity = '0';
+    cargaLetras.innerText = '';
+}
